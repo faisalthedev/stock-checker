4760f478a6909d7c0c839a7c5b8339c17b8b71a7
faisalthedev <faisal13108@gmail.com>
2021-08-04 23:49:41 +0600

    Minor changes, stock recheck every 30s

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const stockUpdate = document.querySelector(".stock-update");
+let stockMessage = "";
 
-fetch("https://panel.bangladeshdeal.com/api/product-core/samsung-guru-music-2-20pcs-combo/3052/", { method: "GET" })
-	.then((res) => res.json())
-	.then((data) => stockChecker(data))
-	.catch((error) => location.reload());
-// .catch((error) => console.error("Error:", error));
+setTimeout(() => {
+	fetch("https://panel.bangladeshdeal.com/api/product-core/samsung-guru-music-2-20pcs-combo/3052/", { method: "GET" })
+		.then((res) => res.json())
+		.then((data) => stockChecker(data))
+		.catch((error) => location.reload());
+	// .catch((error) => console.error("Error:", error));
+}, 5000);
 
 function stockChecker(data) {
 	if (data.stock === 0) {
+		stockMessage = `Guru stock out. Rechecking now...`;
+		stockUpdate.innerHTML = stockMessage;
+
 		location.reload();
 	} else {
-		let stockMessage = `<h3>Guru ${data.stock} PCs stock again.</h3>`;
+		stockMessage = `<h3>Guru ${data.stock} PCs stock again.</h3>`;
 		stockUpdate.innerHTML = stockMessage;
-		alert(`Hurry up, Guru PCs stock again.`);
+
+		alert(`Hurry up, Guru ${data.stock} PCs stock again.`);
 	}
 }
