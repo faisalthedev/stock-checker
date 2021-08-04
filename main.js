f4be11c68446b1a02a500bf377a3db3e1a1108af
faisalthedev <faisal13108@gmail.com>
2021-08-05 00:30:41 +0600

    Recheck stock every 30s

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 const stockUpdate = document.querySelector(".stock-update");
 let stockMessage = "";
 
+// Rechecking every 30s
 setTimeout(() => {
 	fetch("https://panel.bangladeshdeal.com/api/product-core/samsung-guru-music-2-20pcs-combo/3052/", { method: "GET" })
 		.then((res) => res.json())
 		.then((data) => stockChecker(data))
 		.catch((error) => location.reload());
 	// .catch((error) => console.error("Error:", error));
-}, 5000);
+}, 30000);
 
 function stockChecker(data) {
 	if (data.stock === 0) {
 		stockMessage = `<h3 class='red'>Guru stock out. Rechecking now...</h3>`;
 		stockUpdate.innerHTML = stockMessage;
 
-		location.reload();
+		// Reload every 10s
+		setTimeout(() => {
+			location.reload();
+		}, 10000);
 	} else {
 		stockMessage = `<h3 class='green'>Guru ${data.stock} PCs stock again.</h3>`;
 		stockUpdate.innerHTML = stockMessage;
